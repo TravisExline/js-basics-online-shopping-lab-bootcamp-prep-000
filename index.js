8a55eff18695956da412fa0edecd1cd8efc9374a
Travis Exline <t.exline22@gmail.com>
2019-04-10 19:46:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,6 +21,9 @@ function viewCart() {
   if(cart.length === 0) {
     console.log("Your shopping cart is empty.")
   }
+  else{
+    console.log(`In your cart, you have ${getCart()[0].itemName} at $${getCart()[0].itemPrice}.`)
+  }
 }
 
 function total() {
