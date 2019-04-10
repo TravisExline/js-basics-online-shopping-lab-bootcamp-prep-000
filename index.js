140091e98364ff005d76094e0abce27dbf5a93f0
Travis Exline <t.exline22@gmail.com>
2019-04-10 17:46:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,51 +10,21 @@ function setCart(c) {
 }
 
 function addToCart(item) {
-  var price = Math.floor(Math.random() * 100) + 1;
-  var actualItem = {};
-  actualItem[item] = price;
-  cart.push(actualItem)
-  console.log(item + " has been added to your cart.");
+  
 }
 
 function viewCart() {
-  if (cart.length === 0) {
-    console.log("Your shopping cart is empty.")
-  } else {
-    var items = [];
-    for (var i = 0; i < cart.length; i++) {
-      for (var item in cart[i]) {
-        items.push(item + " at $" + cart[i][item])
-      }
-    }
-    console.log("In your cart, you have " + items.join(", ") + ".");
-  }
+
 }
 
 function total() {
-  let t = 0
-  return t
+  // write your code here
 }
 
 function removeFromCart(item) {
-  var itemInCart = false
-  for(var i = 0; i < cart.length; i++){
-    if (cart[i].hasOwnProperty(item)) {
-      itemInCart = true;
-      cart.splice(i, 1);
-    }
-  }
-  if (!itemInCart) {
-    console.log("That item is not in your cart.");
-  }
-  return cart
+  // write your code here
 }
 
-function placeOrder(creditCard) {
-  if (!creditCard) {
-    console.log("We don't have a credit card on file for you to place your order.");
-  } else {
-    console.log("Your total cost is $" + total() + ", which will be charged to the card " + creditCard + ".");
-    cart = [];
-  }
+function placeOrder(cardNumber) {
+  // write your code here
 }
