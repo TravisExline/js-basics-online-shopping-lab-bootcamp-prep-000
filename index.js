385b13157f32b474fab1b231cd63e327a66c76c0
Travis Exline <t.exline22@gmail.com>
2019-04-10 20:01:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,13 +9,19 @@ function setCart(c) {
   return cart;
 }
 
-function addToCart(item) {
-  var cost = {price: Math.floor(Math.random()*100)};
-  var newItem = new Object({itemName: item, itemPrice: Math.floor(Math.random()*100)});
-  cart.push(newItem);
-  console.log(`${newItem} has been added to your cart.`);
-  return cart;
+function viewCart() {
+  if (cart.length === 0) {
+    console.log("Your shopping cart is empty.")
+  } else {
+    var items = [];
+    for (var i = 0; i < cart.length; i++) {
+      for (var item in cart[i]) {
+        items.push(item + " at $" + cart[i][item])
+      }
+    }
+    console.log("In your cart, you have " + items.join(", ") + ".");
   }
+}}
 
 function viewCart() {
   if(getCart.length === 0) {
